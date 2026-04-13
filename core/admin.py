27afc5c60347e5ea72ece7177023e78cf2c5bbd7
Yannick CAISE <yannick.caise@gmail.com>
2026-04-13 21:35:02 +0200

    feat(core): add Organization model with single-instance constraint

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Organization
 
-# Register your models here.
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "enterprise_number", "email")
